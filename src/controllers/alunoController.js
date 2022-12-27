55c245eb1e766b6dd18901d6007cecd961fb4155
Richard Ferreira <richardiwnl@outlook.com>
2022-12-27 02:25:37 -0300

    CRUD do Aluno implementado

--- src/controllers/alunoController.js
+++ src/controllers/alunoController.js
@@ -6,6 +6,119 @@ class AlunoController {
 
     res.json(alunos);
   }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.json({
+        status: 'Aluno criado com sucesso',
+        aluno,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({
+        errors: err.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id, {
+        attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+      });
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      return res.json(aluno);
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({
+        errors: err.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id, {
+        attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+      });
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      await aluno.destroy();
+
+      return res.json({
+        status: 'Apagado com sucesso',
+        aluno,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({
+        errors: err.errors.map((e) => e.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id, {
+        attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'peso', 'altura'],
+      });
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      const novoAluno = await aluno.update(req.body);
+
+      return res.json({
+        status: 'Aluno atualizado com sucesso',
+        aluno: novoAluno,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({
+        errors: err.errors.map((e) => e.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
