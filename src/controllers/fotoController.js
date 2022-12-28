1fd2ef96ee79325b4b25d7b1e52d9688111470f0
Richard Ferreira <richardiwnl@outlook.com>
2022-12-28 00:33:08 -0300

    Salvando a foto na base de dados

--- src/controllers/fotoController.js
+++ src/controllers/fotoController.js
@@ -1,20 +1,25 @@
 import multer from 'multer';
 
-import Aluno from '../models/Aluno';
+import Foto from '../models/Foto';
 import multerConfig from '../config/multer';
 
 const upload = multer(multerConfig).single('foto');
 
 class FotoController {
   async store(req, res) {
-    return upload(req, res, (err) => {
+    return upload(req, res, async (err) => {
       if (err) {
         return res.status(400).json({
           errors: [err.code],
         });
       }
 
-      res.json(req.file);
+      const { originalname, filename } = req.file;
+      const { aluno_id } = req.body;
+
+      const foto = await Foto.create({ aluno_id, originalname, filename });
+
+      res.json(foto);
     });
   }
 }
